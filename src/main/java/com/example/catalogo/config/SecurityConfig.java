12b89e4b62a111b303e606755fc7b4131926f329
Jau Italo <jau.batista@dcx.ufpb.br>
2025-09-12 20:04:18 -0300

    implementano algum tipo de segurança, falta corrigir erros

--- src/main/java/com/example/catalogo/config/SecurityConfig.java
+++ src/main/java/com/example/catalogo/config/SecurityConfig.java
@@ -1,4 +1,58 @@
 package com.example.catalogo.config;
 
+
+import com.example.catalogo.security.jwtAuthFilter;
+import com.example.catalogo.security.jwtAuthentication;
+import com.example.catalogo.service.customUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired private jwtAuthFilter jwtAuthFilter;
+    @Autowired private jwtAuthentication jwtAuthentication;
+    @Autowired private customUserService customUserService;
+
+
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration cofig)
+        throws Exception{
+        return cofig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+        throws Exception {
+        http.csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthentication).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "h2-console/**").permitAll()
+                .requestMatchers("/api/v1/frutas/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        http.headers().frameOptions().disable();
+        return http.build();
+    }
 }
