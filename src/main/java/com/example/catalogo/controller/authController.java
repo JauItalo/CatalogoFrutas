5fed9f6bed65f2b70f69a6ee8b7757fa6e1a1735
Jau Italo <jau.batista@dcx.ufpb.br>
2025-09-15 18:15:42 -0300

    auth feito, mas com erros, corrigirei mais para frente

---
+++ src/main/java/com/example/catalogo/controller/authController.java
@@ -0,0 +1,61 @@
+package com.example.catalogo.controller;
+
+import com.example.catalogo.dto.*;
+import com.example.catalogo.model.Role;
+import com.example.catalogo.model.User;
+import com.example.catalogo.repository.roleRepository;
+import com.example.catalogo.repository.userRepository;
+import com.example.catalogo.security.jwtUtil;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+@RestController
+@RequestMapping("api/v1/auth")
+public class authController {
+
+    @Autowired private AuthenticationManager authenticationManager;
+    @Autowired private jwtUtil jwtUtil;
+    @Autowired private userRepository userRepository;
+    @Autowired private roleRepository roleRepository;
+    @Autowired private PasswordEncoder passwordEncoder;
+
+
+    @PostMapping("/register")
+
+    public ResponseEntity<?> register(@Valid @RequestBody userRegisterDTO dto) {
+        if(userRepository.existsByUsername(dto.getUsername())) return ResponseEntity
+                .badRequest().body("Username em uso");
+        User u = new User();
+        u.setNome(dto.getNome()); u.setUsername(dto.getUsername());
+        u.setPassword(passwordEncoder.encode(dto.getPassword()));
+        Role role = roleRepository.findByName("ROLE_USER").orElseGet(()-> {
+            Role r = new Role(); r.setName("ROLE_USER");
+            return roleRepository.save(r);
+        });
+
+        u.setRoles(Collections.singleton(role));
+        userRepository.save(u);
+        return ResponseEntity.ok("Usuario registrado");
+    }
+   @PostMapping("/login")
+   public ResponseEntity<?> login(@RequestBody loginRequestDTO req){
+       Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+               req.getUsername(), req.getPassword()));
+       List<String> roles = auth.getAuthorities().stream().map(a -> a.getAuthority())
+               .collect(Collectors.toList());
+       String token = jwtUtil.generateToken(req.getUsername(), roles);
+       return ResponseEntity.ok(new loginResponseDTO(token, req.getUsername(), roles));
+   }
+
+   @GetMapping("/me")
+    public ResponseEntity<?> me(@AuthenticationPrincipal org.springframework.security.core.userdetails.User user) {
+        if (user == null) return ResponseEntity.status(401).build();
+        return ResponseEntity.ok(user.getUsername());
+   }
+}
